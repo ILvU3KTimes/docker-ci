711ba0644f81c3aea9a9a13fc5b9ca9509fc955c
wangbin <bin841374040@qq.com>
2025-10-20 15:19:54 +0800

    first commit

---
+++ dist/front.config.js
@@ -0,0 +1,20 @@
+// minio配置项
+var minioConfig = {
+  endPoint: 'picture.zifuture.com',
+  port: 15880,
+  useSSL: false,
+  accessKey: 'admin',
+  secretKey: 'admin123',
+}
+// 系统标题（登录页、管理页）
+// var systemTitle = "AI视频监控与报警管理平台";
+// 页面底部copyRight
+var copyRight = " 2021-2022 Qingdao 青岛港供电有限公司";
+// 监控页面标题
+var monitorTitle = "AI视频监控与报警应用平台";
+// 菜单栏顶部标题-四个字最优
+var menuTitle = "";
+// 菜单栏顶部副标题
+var menuSubTitle = "Qingdao";
+// 临时服务器地址
+var IPAddress = 'http://192.168.99.223'
